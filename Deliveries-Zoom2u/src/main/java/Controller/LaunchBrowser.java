a0ecf9c0a78c0a6391cff6cf9a45a07ccb3681ac
Milind kale <milindkalepersonal014@gmail.com>
2018-09-10 17:37:03 +0530

    browser file updated.

--- Deliveries-Zoom2u/src/main/java/Controller/LaunchBrowser.java
+++ Deliveries-Zoom2u/src/main/java/Controller/LaunchBrowser.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import CommonLibraries.DriverClass;
 import CommonLibraries.Login;
@@ -12,17 +17,20 @@ public class LaunchBrowser extends DriverClass {
    static Login obj1=new Login();
 
 
-	public static void launchbrowser() throws InterruptedException
+	public static void launchbrowser() throws InterruptedException, MalformedURLException
 	{
 		
 		 System.out.println("launching chrome browser");
-		   System.setProperty("webdriver.chrome.driver",obj1.driverpath() + "\\chromedriver.exe");
-		   driver=new ChromeDriver();
+		   //System.setProperty("webdriver.chrome.driver",obj1.driverpath() + "\\chromedriver.exe");
+		   //driver=new ChromeDriver();
+		 DesiredCapabilities capability = DesiredCapabilities.chrome();
+		 WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8080/wd/hub"), capability);
+	        driver.get(obj1.url("https://deliveries.zoom2u.com"));
+		 String page = driver.getPageSource();
 		   driver.manage().timeouts().implicitlyWait(3600, TimeUnit.SECONDS);
 	        
 	        
 	        
-	        driver.get(obj1.url("https://deliveries.zoom2u.com"));
 			   driver.manage().window().maximize();
 			   //Thread.sleep(10000);
 	
