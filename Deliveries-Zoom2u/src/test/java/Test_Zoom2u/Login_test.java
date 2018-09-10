52eeab400d00f7cfde3623040df8f8ca7bbdec06
Milind kale <milindkalepersonal014@gmail.com>
2018-09-10 20:57:32 +0530

    login test file updated...

--- Deliveries-Zoom2u/src/test/java/Test_Zoom2u/Login_test.java
+++ Deliveries-Zoom2u/src/test/java/Test_Zoom2u/Login_test.java
@@ -22,23 +22,30 @@ public class Login_test extends LaunchBrowser{
   public void L1() throws InterruptedException{
     
 	 obj2.loginwithvalidcredentials("keenal0904@gmail.com","123456");
+	 Thread.sleep(10000);
 	
 	}
 	
 	@Test (priority=3)
 	public void L4() throws InterruptedException{
 		obj2.loginwithinvalidcredentials("miiind@gmail.com", "1234567");
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=4)
 	public void L5() throws InterruptedException{
 		obj2.loginwithoutcredentials();
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=5)
 	public void L2() throws InterruptedException{
 		
 		obj2.loginwithinvalidpassword("keenal0904@gmail.com", "123456789");
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=6)
@@ -48,30 +55,37 @@ public class Login_test extends LaunchBrowser{
 		obj2.loginwithinvalidcredentials("milindff@gmail.com", "123456");
 		System.out.println("Login with invalid username and valid pwd 3rd test case completed");
 		System.out.println("...........................................................................................................");
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=7)
 	public void L6() throws InterruptedException{
 		obj2.MaskedPwdfield();
-		
+		 Thread.sleep(10000);
 	}
 	
 	@Test (priority=8)
     public void L7() throws InterruptedException{
 		obj2.redirectTosameloginurlOnRefresh();
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=1)
 	public void L8() throws InterruptedException 
 	{
 		obj2.focusonloginsection();
+		 Thread.sleep(10000);
+
 	}
 	
 	@Test (priority=9)
 	public void L9() throws InterruptedException 
 	{
 		obj2.verifydisableaccounttext("testcustomer03@gmail.com", "123456");
-	}
+		 Thread.sleep(10000);
+}
 	
 	
   @BeforeTest
@@ -81,8 +95,8 @@ public class Login_test extends LaunchBrowser{
 	 //LaunchBrowser_inCrossBrowser.OpenBrowser("Firefox", "55*64", "Windows 7 64-bit", "LoginModule_Test");
 	 //LaunchBrowser_inCrossBrowser.OpenBrowser("Internet Explorer", "11", "Windows 7 64-bit", "LoginModule_Test");
 	 launchbrowser();
-	  
-  }
+	 Thread.sleep(10000);
+ }
   
   @AfterTest
   public void afterTest() throws InterruptedException {
